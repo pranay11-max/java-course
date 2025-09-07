69e82945f67748e6e2b2f215d5232e75d19ced83
Ajit1978 <munjajit@gmail.com>
2025-09-07 13:01:02 +0530

    patterns

---
+++ practice/triangle3.java
@@ -0,0 +1,15 @@
+public class triangle3{
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++){
+            for(int j=i;j<=5;j++){
+                System.out.print("  ");
+            }
+            for(int k=0;k<i*2+1;k++){
+                System.out.print("* ");
+            }
+
+            
+            System.out.println();
+        }
+    }
+}
